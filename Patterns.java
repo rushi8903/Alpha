05ea93e12a4e7b645b2de4fab79ef4c18caa569d
Rushikesh Chaure <113259190+rushi8903@users.noreply.github.com>
2023-03-28 23:53:57 +0530

    Update Patterns.java
    adding four more types of patterns to it.

--- Patterns.java
+++ Patterns.java
@@ -124,10 +124,128 @@ public class Patterns {
         }
         sc.close();
     }
+    
+    static void butterfly(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of butterfly :");
+        int height = sc.nextInt();
+
+        for(int i=1;i<=2*height;i++){
+            for(int j=1;j<=2*height;j++){
+                if(i<=height && j<=height){
+                    if(i>=j){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }else if(i<=height && j>height){
+                    if(i+j>=2*height+1){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }else if(i>height && j<=height){
+                    if(i+j<=2*height+1){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }else{
+                    if(i<=j){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    static void solidRhombus(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height of a Rhombus :");
+        int height = sc.nextInt();
+
+        for(int i=1;i<=height;i++){
+            for(int j=1;j<2*height;j++){
+                if(i+j<=height){
+                    System.out.print("  ");
+                }else if(i+j>2*height){
+                    System.out.print("  ");
+                }else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    static void hollowRhombus(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height of a Rhombus :");
+        int height = sc.nextInt();
+        int n= height-1;
+
+        for(int i=1;i<=height;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("  ");
+            }
+            n--;
+            System.out.print("* ");
+            for(int j=1;j<=height-2;j++){
+                if(i==1 || i==height){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("* ");
+            System.out.println();
+        }       
+        sc.close();
+    }
+
+    static void diamand(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height of a pyramid :");
+        int height = sc.nextInt();
+        int n=height;
+
+        for(int i=1;i<=height;i++){
+            for(int j=1;j<=n-1;j++){
+                System.out.print("  ");
+            }
+            n--;
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+        height--;
+        n=height;
+        for(int i=1;i<=height;i++){
+            System.out.print("  ");
+            for(int j=0;j<i;j++){
+                if(j!=0){
+                System.out.print("  ");
+                }
+            }
+            for(int j=1;j<=2*n-1;j++){
+                System.out.print("* ");
+            }
+            n--;
+            System.out.println(); 
+        }
+
+        sc.close();
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Types of patterns :\n\t1. Hollow rectangle\n\t2. Left half-pyramid\n\t3. Right half-pyramid\n\t4. Inverted left half-pyramid with numbers\n\t5. Inverted right half-pyramid with numbers\n\t6. FLOYD'S pyramid\n\t7. Zero one triangle");
+        System.out.println("Types of patterns :\n\t1. Hollow rectangle\n\t2. Left half-pyramid\n\t3. Right half-pyramid\n\t4. Inverted left half-pyramid with numbers\n\t5. Inverted right half-pyramid with numbers\n\t6. FLOYD'S pyramid\n\t7. Zero one triangle\n\t8. Butterfly\n\t9. Solid Rhombus\n\t10. Hollow Rhombus\n\t11. Diamand");
         System.out.println("Enter the choice :");
         int choice = sc.nextInt();
         switch(choice){
@@ -151,6 +269,18 @@ public class Patterns {
                 break;
             case 7:
                 zeroOneTriangle();
+                break;
+            case 8:
+                butterfly();
+                break;
+            case 9:
+                solidRhombus();
+                break;
+            case 10:
+                hollowRhombus();
+                break;
+            case 11:
+                diamand();
                 break;
             default:
                 System.out.println("Enter valid choice.");
